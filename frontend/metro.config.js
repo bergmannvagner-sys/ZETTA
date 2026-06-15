6b48c653c175fa3957602570b982e2c15ecf501b
bergmannvagner-sys <bergmannvagner@gmail.com>
2026-06-15 13:00:17 -0300

    frontend: stabilize metro for local native package mirror
    Point Metro and autolinking at the local native-package mirror and keep the copied package tree out of TypeScript so web export and Android bundling can resolve the Expo native stack cleanly.

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,18 @@
+const path = require("path");
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const nativePackagesRoot = path.resolve(__dirname, "native-packages");
+
+config.watchFolders = [...(config.watchFolders ?? []), nativePackagesRoot];
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules ?? {}),
+  "expo-modules-core": path.join(nativePackagesRoot, "expo-modules-core"),
+  "react-native-gesture-handler": path.join(nativePackagesRoot, "react-native-gesture-handler"),
+  "react-native-reanimated": path.join(nativePackagesRoot, "react-native-reanimated"),
+  "react-native-screens": path.join(nativePackagesRoot, "react-native-screens")
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
